0f3396cb38be152ce71ad4d1acdd2cfc229e1280
AdelineDb <adeline.dubosc@hotmail.fr>
2019-04-09 15:44:06 +0200

    resultat quete

---
+++ Twig/public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+
+//initialise et intègre twig
+$loader = new Twig\Loader\FilesystemLoader(__DIR__.'/../src/View');
+$twig = new Twig\Environment($loader);
+echo $twig->render('index.html.twig',['products' => $products]);
+
